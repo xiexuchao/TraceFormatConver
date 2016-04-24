369680e91619e81ba54ed09beb42d75e72599da1
xiexuchao <xiexuchao@foxmail.com>
2016-04-24 10:32:02 -0500

    add support of msr trace

--- TraceFormatConver.c
+++ TraceFormatConver.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-//#define __TRACE_MSR__
+#define __TRACE_MSR__
 //#define __TRACE_SPC__
 //#define __TRACE_NETAPP__
 
@@ -9,7 +9,8 @@
 #define READ	0
 #define WRITE	1
 
-#define TIMESLOT 5 //MIN
+#define SLOT_BEG 55 //MIN
+#define SLOT_END 65
 
 void msr2ascii(char *source,char *target);
 void netapp2ascii(char *source,char *target);
@@ -18,19 +19,59 @@ void main()
 {
 #ifdef __TRACE_MSR__
 	char path_i[100]="F:\\MSR Trace\\";
+	char path_o[100]="F:\\MSR Trace\\";
 #else
 	char path_i[100]="F:\\Netapp Trace\\";
+	char path_o[100]="F:\\Netapp Trace\\";
 #endif
-	char path_o[100]="results\\";
 	char path_source[100];
 	char path_target[100];
 
 #ifdef __TRACE_MSR__
+	msr2ascii(strcat(strcpy(path_source,path_i),"hm_0.csv"),strcat(strcpy(path_target,path_o),"hm_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"hm_1.csv"),strcat(strcpy(path_target,path_o),"hm_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"mds_0.csv"),strcat(strcpy(path_target,path_o),"mds_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"mds_1.csv"),strcat(strcpy(path_target,path_o),"mds_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"prn_0.csv"),strcat(strcpy(path_target,path_o),"prn_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"prn_1.csv"),strcat(strcpy(path_target,path_o),"prn_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"proj_0.csv"),strcat(strcpy(path_target,path_o),"proj_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"proj_1.csv"),strcat(strcpy(path_target,path_o),"proj_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"proj_2.csv"),strcat(strcpy(path_target,path_o),"proj_2.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"proj_3.csv"),strcat(strcpy(path_target,path_o),"proj_3.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"proj_4.csv"),strcat(strcpy(path_target,path_o),"proj_4.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"prxy_0.csv"),strcat(strcpy(path_target,path_o),"prxy_0.ascii"));
 	msr2ascii(strcat(strcpy(path_source,path_i),"prxy_1.csv"),strcat(strcpy(path_target,path_o),"prxy_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"rsrch_0.csv"),strcat(strcpy(path_target,path_o),"rsrch_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"rsrch_1.csv"),strcat(strcpy(path_target,path_o),"rsrch_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"rsrch_2.csv"),strcat(strcpy(path_target,path_o),"rsrch_2.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"src1_0.csv"),strcat(strcpy(path_target,path_o),"src1_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"src1_1.csv"),strcat(strcpy(path_target,path_o),"src1_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"src1_2.csv"),strcat(strcpy(path_target,path_o),"src1_2.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"src2_0.csv"),strcat(strcpy(path_target,path_o),"src2_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"src2_1.csv"),strcat(strcpy(path_target,path_o),"src2_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"src2_2.csv"),strcat(strcpy(path_target,path_o),"src2_2.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"stg_0.csv"),strcat(strcpy(path_target,path_o),"stg_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"stg_1.csv"),strcat(strcpy(path_target,path_o),"stg_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"ts_0.csv"),strcat(strcpy(path_target,path_o),"ts_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"usr_0.csv"),strcat(strcpy(path_target,path_o),"usr_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"usr_1.csv"),strcat(strcpy(path_target,path_o),"usr_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"usr_2.csv"),strcat(strcpy(path_target,path_o),"usr_2.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"wdev_0.csv"),strcat(strcpy(path_target,path_o),"wdev_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"wdev_1.csv"),strcat(strcpy(path_target,path_o),"wdev_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"wdev_2.csv"),strcat(strcpy(path_target,path_o),"wdev_2.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"wdev_3.csv"),strcat(strcpy(path_target,path_o),"wdev_3.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"web_0.csv"),strcat(strcpy(path_target,path_o),"web_0.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"web_1.csv"),strcat(strcpy(path_target,path_o),"web_1.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"web_2.csv"),strcat(strcpy(path_target,path_o),"web_2.ascii"));
+	msr2ascii(strcat(strcpy(path_source,path_i),"web_3.csv"),strcat(strcpy(path_target,path_o),"web_3.ascii"));
 #else
-	netapp2ascii(strcat(strcpy(path_source,path_i),"UMNtrace2_5.csv"),strcat(strcpy(path_target,path_o),"UMNtrace2_5.ascii"));
+	netapp2ascii(strcat(strcpy(path_source,path_i),"hadoop10.csv"),strcat(strcpy(path_target,path_o),"hadoop10.ascii"));
+	netapp2ascii(strcat(strcpy(path_source,path_i),"hadoop13.csv"),strcat(strcpy(path_target,path_o),"hadoop13.ascii"));
+	netapp2ascii(strcat(strcpy(path_source,path_i),"backup1.csv"),strcat(strcpy(path_target,path_o),"backup1.ascii"));
+	netapp2ascii(strcat(strcpy(path_source,path_i),"backup5.csv"),strcat(strcpy(path_target,path_o),"backup5.ascii"));
+	netapp2ascii(strcat(strcpy(path_source,path_i),"backup14.csv"),strcat(strcpy(path_target,path_o),"backup14.ascii"));
+	netapp2ascii(strcat(strcpy(path_source,path_i),"backup15.csv"),strcat(strcpy(path_target,path_o),"backup15.ascii"));
 #endif
-
 }
 
 void msr2ascii(char *source,char *target)
@@ -106,7 +147,6 @@ void msr2ascii(char *source,char *target)
 	fclose(file_target);
 }
 
-
 void netapp2ascii(char *source,char *target)
 {
 	char buf[BUFSIZE];
@@ -158,7 +198,7 @@ void netapp2ascii(char *source,char *target)
 			printf("processing %d ...\n",count);
 		}
 		elapsedTime=elapsedTime-initTime;
-		
+
 		//Format Covert
 		time=(double)(elapsedTime/1000); //us-->ms
 		dev=lunssid;
